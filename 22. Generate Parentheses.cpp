b0299bd4813ba5cbae874475d3d98e058814dd9c
mdsaquibabbas313 <120925424+mdsaquibabbas313@users.noreply.github.com>
2025-09-13 12:08:24 +0530

    22. Generate Parentheses
    https://leetcode.com/problems/generate-parentheses/description/

---
+++ 22. Generate Parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void solve(int open , int close , string op , vector<string>& ans) {
+        if(open == 0 && close == 0) {
+            ans.push_back(op);
+            return ;
+        }
+
+
+        if(open != 0) {
+            string op1 = op;
+            op1 += "(";
+            solve(open-1 , close , op1 , ans);
+        }
+
+        if(close > open) {
+            string op2 = op;
+            op2 += ")";
+            solve(open , close-1 , op2 , ans);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+
+        string op = "";
+        int open = n;
+        int close = n;
+
+        solve(open, close,op , ans);
+
+        return ans;
+    }
+};
